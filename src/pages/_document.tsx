9bdb0b4d6b967569bd4b78a5f90412c684bcf647
Jesse Pinho <jesse@jessepinho.com>
2023-02-28 13:47:47 -0800

    Add meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <body>
         <Main />
