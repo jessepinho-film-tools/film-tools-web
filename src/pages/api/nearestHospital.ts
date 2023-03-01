fec89662af56486d8b227b492f8a1619d8d7a938
Jesse Pinho <jesse@jessepinho.com>
2023-03-01 14:43:42 -0800

    Add an endpoint for the nearest hospital

---
+++ src/pages/api/nearestHospital.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const getAddresses = (resultFromGoogle: any) =>
+  resultFromGoogle?.results.map((result: any) => ({
+    name: result.name,
+    address: result.vicinity,
+  }))
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  return fetch(
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.query.latlng}&rankby=distance&type=hospital&key=${process.env.GOOGLE_MAPS_SERVER_SIDE_API_KEY}`
+  )
+    .then((res) => res.json())
+    .then((hospitals) => res.status(200).json(getAddresses(hospitals)))
+    .catch((error) => res.json(error))
+}
