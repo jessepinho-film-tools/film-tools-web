8d7ec91f3ed87a1efd30d3d32141dd5c3419a509
Jesse Pinho <jesse@jessepinho.com>
2023-03-01 12:22:41 -0800

    Build out the forecast endpoint

--- src/pages/api/forecast.ts
+++ src/pages/api/forecast.ts
@@ -1,8 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+type Data =
+  | {
+      highC: number
+      highF: number
+      lowC: number
+      lowF: number
+      sunrise: string
+      sunset: string
+    }
+  | {}
+
+const getSimpleForecastForDate = (forecastData: any, date?: any): Data => {
+  const days = forecastData.forecast.forecastday
+
+  let simpleForecast: Data = {}
+
+  days.forEach((day: any) => {
+    if (day.date !== date) return
+    simpleForecast = {
+      highC: day.day.maxtemp_c,
+      highF: day.day.maxtemp_f,
+      lowC: day.day.mintemp_c,
+      lowF: day.day.mintemp_f,
+      sunrise: day.astro.sunrise,
+      sunset: day.astro.sunset,
+    }
+  })
+
+  return simpleForecast
 }
 
 export default async function handler(
@@ -13,6 +40,10 @@ export default async function handler(
     `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&days=14&q=${req.query.latlng}`
   )
     .then((res) => res.json())
-    .then((forecast) => res.status(200).json(forecast))
+    .then((forecastData) =>
+      res
+        .status(200)
+        .json(getSimpleForecastForDate(forecastData, req.query.date))
+    )
     .catch((error) => res.json(error))
 }
