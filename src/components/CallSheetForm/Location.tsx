f6faa21e0cce888961e49b5250b948b3cc3fefd2
Jesse Pinho <jesse@jessepinho.com>
2023-03-01 15:18:12 -0800

    Build out more of the location feature

---
+++ src/components/CallSheetForm/Location.tsx
@@ -0,0 +1,117 @@
+import {
+  Autocomplete,
+  Box,
+  TableRow,
+  TableCell,
+  TextField,
+  Typography,
+  Grid,
+} from '@mui/material'
+import { Controller, useFormContext, useWatch } from 'react-hook-form'
+import { defineMessages, useIntl } from 'react-intl'
+import { useEffect, useState } from 'react'
+
+import AddressField from './AddressField'
+
+const M = defineMessages({
+  mapImageAltText: {
+    defaultMessage: 'A map of the location',
+    id: 'CallSheetForm.Locations.mapImageAltText',
+  },
+  notesLabel: {
+    defaultMessage: 'Notes',
+    id: 'CallSheetForm.Locations.notesLabel',
+  },
+  nearestHospitalLabel: {
+    defaultMessage: 'Nearest hospital',
+    id: 'CallSheetForm.Location.nearestHospitalLabel',
+  },
+})
+
+export default function Location({
+  field,
+  index,
+}: {
+  field: Record<'id', string>
+  index: number
+}) {
+  const [hospitals, setHospitals] = useState<any[]>([])
+  const { control } = useFormContext()
+  const intl = useIntl()
+  const location = useWatch({ control, name: `locations.${index}` })
+
+  useEffect(() => {
+    if (!location) return
+
+    const params = new URLSearchParams({
+      latlng: `${location.coordinates.lat},${location.coordinates.lng}`,
+    })
+    fetch(`/api/nearestHospital?${params}`)
+      .then((res) => res.json())
+      .then(setHospitals)
+  }, [location])
+
+  return (
+    <TableRow key={field.id}>
+      <TableCell sx={{ verticalAlign: 'top', width: '33%' }}>
+        <Controller
+          control={control}
+          name={`locations.${index}`}
+          defaultValue=""
+          render={({ field }) => (
+            <>
+              <AddressField
+                onChange={field.onChange}
+                value={field.value || ''}
+              />
+
+              {field.value && (
+                <img
+                  style={{ width: '100%' }}
+                  src={`https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=roadmap&zoom=16&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_CLIENT_SIDE_API_KEY}&markers=${field.value.address}`}
+                  alt={intl.formatMessage(M.mapImageAltText)}
+                />
+              )}
+            </>
+          )}
+        />
+      </TableCell>
+
+      <TableCell sx={{ verticalAlign: 'top', width: '33%' }}>
+        <TextField
+          variant="standard"
+          label={intl.formatMessage(M.notesLabel)}
+          multiline
+          fullWidth
+        />
+      </TableCell>
+
+      <TableCell sx={{ verticalAlign: 'top', width: '33%' }}>
+        <Autocomplete
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="standard"
+              label={intl.formatMessage(M.nearestHospitalLabel)}
+              fullWidth
+            />
+          )}
+          options={hospitals}
+          getOptionLabel={(option) => option.name}
+          renderOption={(props, option) => {
+            return (
+              <Grid component="li" container {...props}>
+                <Grid item xs={12} sx={{ fontWeight: 'bold' }}>
+                  {option.name}
+                </Grid>
+                <Grid item xs={12}>
+                  {option.address}
+                </Grid>
+              </Grid>
+            )
+          }}
+        />
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/components/CallSheetForm/Locations.tsx
+++ src/components/CallSheetForm/Locations.tsx
@@ -7,44 +7,20 @@ import {
   IconButton,
   Table,
   TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
 } from '@mui/material'
-import { Controller, useFieldArray, useFormContext } from 'react-hook-form'
 import { defineMessages, useIntl } from 'react-intl'
+import { useFieldArray, useFormContext } from 'react-hook-form'
 
-import AddressField from './AddressField'
+import Location from './Location'
 
 const M = defineMessages({
   locationsTitle: {
     defaultMessage: 'Location(s)',
     id: 'CallSheetForm.Locations.locationsTitle',
   },
-  locationHeader: {
-    defaultMessage: 'Location',
-    id: 'CallSheetForm.Locations.locationHeader',
-  },
-  notesHeader: {
-    defaultMessage: 'Parking & notes',
-    id: 'CallSheetForm.Locations.notesHeader',
-  },
-  nearestHospitalHeader: {
-    defaultMessage: 'Nearest hospital',
-    id: 'CallSheetForm.Locations.nearestHospitalHeader',
-  },
-  mapImageAltText: {
-    defaultMessage: 'A map of the location',
-    id: 'CallSheetForm.Locations.mapImageAltText',
-  },
   addButtonLabel: {
     defaultMessage: 'Add another location',
     id: 'CallSheetForm.Locations.addButtonLabel',
-  },
-  notesLabel: {
-    defaultMessage: 'Notes',
-    id: 'CallSheetForm.Locations.notesLabel',
   },
 })
 
@@ -64,51 +40,9 @@ export default function Locations() {
 
       <CardContent>
         <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>{intl.formatMessage(M.locationHeader)}</TableCell>
-              <TableCell>{intl.formatMessage(M.notesHeader)}</TableCell>
-              <TableCell>
-                {intl.formatMessage(M.nearestHospitalHeader)}
-              </TableCell>
-            </TableRow>
-          </TableHead>
           <TableBody>
             {fields.map((field, index) => (
-              <TableRow key={field.id}>
-                <TableCell sx={{ verticalAlign: 'top', width: '33%' }}>
-                  <Controller
-                    control={control}
-                    name={`locations.${index}`}
-                    defaultValue=""
-                    render={({ field }) => (
-                      <>
-                        <AddressField
-                          onChange={field.onChange}
-                          value={field.value || ''}
-                        />
-
-                        {field.value && (
-                          <img
-                            style={{ width: '100%' }}
-                            src={`https://maps.googleapis.com/maps/api/staticmap?size=400x200&maptype=roadmap&zoom=16&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_CLIENT_SIDE_API_KEY}&markers=${field.value.address}`}
-                            alt={intl.formatMessage(M.mapImageAltText)}
-                          />
-                        )}
-                      </>
-                    )}
-                  />
-                </TableCell>
-
-                <TableCell sx={{ verticalAlign: 'top', width: '33%' }}>
-                  <TextField
-                    variant="standard"
-                    label={intl.formatMessage(M.notesLabel)}
-                    multiline
-                    fullWidth
-                  />
-                </TableCell>
-              </TableRow>
+              <Location key={field.id} field={field} index={index} />
             ))}
           </TableBody>
         </Table>
