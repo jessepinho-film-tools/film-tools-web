adccd67c7b1e971970fe46d9cc8745591de1feac
Jesse Pinho <jesse@jessepinho.com>
2023-03-01 22:52:55 -0800

    Move production name to top; add call time field

--- src/components/CallSheetForm/index.tsx
+++ src/components/CallSheetForm/index.tsx
@@ -22,6 +22,10 @@ const M = defineMessages({
     defaultMessage: 'Date',
     id: 'CallSheetForm.dateLabel',
   },
+  callTimeLabel: {
+    defaultMessage: 'General call time',
+    id: 'CallSheetForm.callTimeLabel',
+  },
 })
 
 export default function CallSheetForm({ callSheet }: { callSheet?: any }) {
@@ -32,6 +36,16 @@ export default function CallSheetForm({ callSheet }: { callSheet?: any }) {
   return (
     <FormProvider {...methods}>
       <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            label={intl.formatMessage(M.productionNameLabel)}
+            inputProps={register('production.name', { required: true })}
+            placeholder={intl.formatMessage(M.productionNamePlaceholder)}
+            variant="standard"
+            fullWidth
+            sx={{ mb: 2 }}
+          />
+        </Grid>
         <Grid item xs={12} sm={8}>
           <Card>
             <CardContent>
@@ -39,22 +53,27 @@ export default function CallSheetForm({ callSheet }: { callSheet?: any }) {
                 {intl.formatMessage(M.basicInfoTitle)}
               </Typography>
 
-              <TextField
-                label={intl.formatMessage(M.productionNameLabel)}
-                inputProps={register('production.name', { required: true })}
-                placeholder={intl.formatMessage(M.productionNamePlaceholder)}
-                variant="standard"
-                fullWidth
-                sx={{ mb: 2 }}
-              />
-
-              <TextField
-                type="date"
-                label={intl.formatMessage(M.dateLabel)}
-                inputProps={register('date', { required: true })}
-                variant="standard"
-                fullWidth
-              />
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    type="date"
+                    label={intl.formatMessage(M.dateLabel)}
+                    inputProps={register('date', { required: true })}
+                    variant="standard"
+                    fullWidth
+                    sx={{ mb: 2 }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    type="time"
+                    label={intl.formatMessage(M.callTimeLabel)}
+                    inputProps={register('callTime', { required: true })}
+                    variant="standard"
+                    fullWidth
+                  />
+                </Grid>
+              </Grid>
             </CardContent>
           </Card>
         </Grid>
